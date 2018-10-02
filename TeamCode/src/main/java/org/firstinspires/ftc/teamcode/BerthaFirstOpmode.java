631a9f2a531508048dc059571a8e8ae3ac661fb9
Bertha Mireles <mirelesb20@themsms.org>
2018-10-02 15:30:31 -0500

    created a new file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BerthaFirstOpmode.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BerthaFirstOpmode", group="Linear Opmode")
+
+public class BerthaFirstOpmode extends LinearOpMode
+{ @Override
+    public void runOpMode (){}
+}
